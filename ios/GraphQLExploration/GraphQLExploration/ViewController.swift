283f77d3c837b7d0862ca4cb79f0e963575da86a
John Garrett <jgarrett@kabbage.com>
2019-06-18 14:43:43 -0400

    inital setup

---
+++ ios/GraphQLExploration/GraphQLExploration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphQLExploration
+//
+//  Created by John Garrett on 6/18/19.
+//  Copyright © 2019 John Garrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
