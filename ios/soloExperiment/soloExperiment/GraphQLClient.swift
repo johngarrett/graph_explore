5096123426cd827db358544fd565afe7e03cdea1
John Garrett <jgarrett@kabbage.com>
2019-06-18 16:30:04 -0400

    initalize graphcool server

---
+++ ios/soloExperiment/soloExperiment/GraphQLClient.swift
@@ -0,0 +1,8 @@
+import Foundation
+import Apollo
+
+public let apollo = ApolloClient(url: URL(string: "https://api.graph.cool/simple/v1/cjx26uwx149dq0139kxq7rccz")!)
+
+class GraphQLClient {
+    
+}
