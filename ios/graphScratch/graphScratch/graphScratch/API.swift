1fd57d07e19035606179850078fedd9ce82d3caa
John Garrett <jgarrett@kabbage.com>
2019-06-19 09:47:27 -0400

    finish building graphQL interface

--- ios/graphScratch/graphScratch/graphScratch/API.swift
+++ ios/graphScratch/graphScratch/graphScratch/API.swift
@@ -51,8 +51,8 @@ public final class NotesDetailsQuery: GraphQLQuery {
         self.resultMap = unsafeResultMap
       }
 
-      public init(id: GraphQLID, title: String? = nil, creationDate: String? = nil) {
-        self.init(unsafeResultMap: ["__typename": "Note", "id": id, "title": title, "creationDate": creationDate])
+      public init(id: GraphQLID, title: String? = nil, body: String? = nil, creationDate: String? = nil) {
+        self.init(unsafeResultMap: ["__typename": "Note", "id": id, "title": title, "body": body, "creationDate": creationDate])
       }
 
       public var __typename: String {
@@ -93,9 +93,177 @@ public final class NotesDetailsQuery: GraphQLQuery {
   }
 }
 
+public final class CreateNoteMutation: GraphQLMutation {
+  public let operationDefinition =
+    "mutation CreateNote($title: String!, $body: String!, $creationDate: DateTime!) {\n  createNote(title: $title, body: $body, creationDate: $creationDate) {\n    __typename\n    id\n  }\n}"
+
+  public var title: String
+  public var body: String
+  public var creationDate: String
+
+  public init(title: String, body: String, creationDate: String) {
+    self.title = title
+    self.body = body
+    self.creationDate = creationDate
+  }
+
+  public var variables: GraphQLMap? {
+    return ["title": title, "body": body, "creationDate": creationDate]
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes = ["Mutation"]
+
+    public static let selections: [GraphQLSelection] = [
+      GraphQLField("createNote", arguments: ["title": GraphQLVariable("title"), "body": GraphQLVariable("body"), "creationDate": GraphQLVariable("creationDate")], type: .object(CreateNote.selections)),
+    ]
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(createNote: CreateNote? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Mutation", "createNote": createNote.flatMap { (value: CreateNote) -> ResultMap in value.resultMap }])
+    }
+
+    public var createNote: CreateNote? {
+      get {
+        return (resultMap["createNote"] as? ResultMap).flatMap { CreateNote(unsafeResultMap: $0) }
+      }
+      set {
+        resultMap.updateValue(newValue?.resultMap, forKey: "createNote")
+      }
+    }
+
+    public struct CreateNote: GraphQLSelectionSet {
+      public static let possibleTypes = ["Note"]
+
+      public static let selections: [GraphQLSelection] = [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+      ]
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(id: GraphQLID) {
+        self.init(unsafeResultMap: ["__typename": "Note", "id": id])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var id: GraphQLID {
+        get {
+          return resultMap["id"]! as! GraphQLID
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "id")
+        }
+      }
+    }
+  }
+}
+
+public final class UpdateNoteMutation: GraphQLMutation {
+  public let operationDefinition =
+    "mutation UpdateNote($id: ID!, $title: String!, $body: String!, $creationDate: DateTime!) {\n  updateNote(id: $id, title: $title, body: $body, creationDate: $creationDate) {\n    __typename\n    id\n  }\n}"
+
+  public var id: GraphQLID
+  public var title: String
+  public var body: String
+  public var creationDate: String
+
+  public init(id: GraphQLID, title: String, body: String, creationDate: String) {
+    self.id = id
+    self.title = title
+    self.body = body
+    self.creationDate = creationDate
+  }
+
+  public var variables: GraphQLMap? {
+    return ["id": id, "title": title, "body": body, "creationDate": creationDate]
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes = ["Mutation"]
+
+    public static let selections: [GraphQLSelection] = [
+      GraphQLField("updateNote", arguments: ["id": GraphQLVariable("id"), "title": GraphQLVariable("title"), "body": GraphQLVariable("body"), "creationDate": GraphQLVariable("creationDate")], type: .object(UpdateNote.selections)),
+    ]
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(updateNote: UpdateNote? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Mutation", "updateNote": updateNote.flatMap { (value: UpdateNote) -> ResultMap in value.resultMap }])
+    }
+
+    public var updateNote: UpdateNote? {
+      get {
+        return (resultMap["updateNote"] as? ResultMap).flatMap { UpdateNote(unsafeResultMap: $0) }
+      }
+      set {
+        resultMap.updateValue(newValue?.resultMap, forKey: "updateNote")
+      }
+    }
+
+    public struct UpdateNote: GraphQLSelectionSet {
+      public static let possibleTypes = ["Note"]
+
+      public static let selections: [GraphQLSelection] = [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+      ]
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(id: GraphQLID) {
+        self.init(unsafeResultMap: ["__typename": "Note", "id": id])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var id: GraphQLID {
+        get {
+          return resultMap["id"]! as! GraphQLID
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "id")
+        }
+      }
+    }
+  }
+}
+
 public struct NoteFullDetails: GraphQLFragment {
   public static let fragmentDefinition =
-    "fragment NoteFullDetails on Note {\n  __typename\n  id\n  title\n  id\n  creationDate\n}"
+    "fragment NoteFullDetails on Note {\n  __typename\n  id\n  title\n  body\n  id\n  creationDate\n}"
 
   public static let possibleTypes = ["Note"]
 
@@ -103,6 +271,7 @@ public struct NoteFullDetails: GraphQLFragment {
     GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
     GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
     GraphQLField("title", type: .scalar(String.self)),
+    GraphQLField("body", type: .scalar(String.self)),
     GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
     GraphQLField("creationDate", type: .scalar(String.self)),
   ]
@@ -113,8 +282,8 @@ public struct NoteFullDetails: GraphQLFragment {
     self.resultMap = unsafeResultMap
   }
 
-  public init(id: GraphQLID, title: String? = nil, creationDate: String? = nil) {
-    self.init(unsafeResultMap: ["__typename": "Note", "id": id, "title": title, "creationDate": creationDate])
+  public init(id: GraphQLID, title: String? = nil, body: String? = nil, creationDate: String? = nil) {
+    self.init(unsafeResultMap: ["__typename": "Note", "id": id, "title": title, "body": body, "creationDate": creationDate])
   }
 
   public var __typename: String {
@@ -141,6 +310,15 @@ public struct NoteFullDetails: GraphQLFragment {
     }
     set {
       resultMap.updateValue(newValue, forKey: "title")
+    }
+  }
+
+  public var body: String? {
+    get {
+      return resultMap["body"] as? String
+    }
+    set {
+      resultMap.updateValue(newValue, forKey: "body")
     }
   }
 
--- ios/graphScratch/graphScratch/graphScratch/GraphAPI.swift
+++ ios/graphScratch/graphScratch/graphScratch/GraphAPI.swift
@@ -3,3 +3,35 @@ import Apollo
 
 fileprivate let simpleURL = URL(string: "https://api.graph.cool/simple/v1/cjx389uec58rs0139zjo300oj")
 let apollo = ApolloClient(url: simpleURL!)
+
+class GraphAPI {
+    
+    func queryAllNotes(_ completion: @escaping (Swift.Result<[NoteFullDetails]?, Error>) -> Void) {        
+        apollo.fetch(query: NotesDetailsQuery(), cachePolicy: .fetchIgnoringCacheData) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            let notes = result?.data?.allNotes.map { $0.fragments.noteFullDetails }
+            completion(.success(notes))
+        }
+    }
+    
+    func addNote(title: String, body: String, creationDate: String, _ completion: @escaping (Error?) -> Void) {
+        let createNoteMutation = CreateNoteMutation(title: title, body: body, creationDate: creationDate)
+        
+        apollo.perform(mutation: createNoteMutation) { _, error in
+            completion(error)
+        }
+    }
+    
+    func updateNote(note: NoteFullDetails, _ completion: @escaping (Error?) -> Void) {
+        let updateNoteMutation = UpdateNoteMutation(id: note.id,
+                                                    title: note.title ?? "null",  //these are non null graphQL values... don't know why the API makes them optional
+                                                    body: note.body ?? "null",
+                                                    creationDate: note.creationDate ?? "null")
+        
+        apollo.perform(mutation: updateNoteMutation) { _, error in
+            completion(error)
+        }
+    }
+}
