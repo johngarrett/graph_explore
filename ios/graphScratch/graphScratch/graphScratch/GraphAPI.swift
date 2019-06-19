37147ee063e6ffa201b5a2b761f60646c071a651
John Garrett <jgarrett@kabbage.com>
2019-06-19 08:50:02 -0400

    begin a new project

---
+++ ios/graphScratch/graphScratch/graphScratch/GraphAPI.swift
@@ -0,0 +1,4 @@
+import Foundation
+import Apollo
+
+let apollo = ApolloClient(url: URL(string: "https://api.graph.cool/simple/v1/cjx389uec58rs0139zjo300oj")!)
--- ios/soloExperiment/soloExperiment/GraphQLClient.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import Apollo
-
-public let apollo = ApolloClient(url: URL(string: "https://api.graph.cool/simple/v1/cjx26uwx149dq0139kxq7rccz")!)
-
-class GraphQLClient {
-    
-}
